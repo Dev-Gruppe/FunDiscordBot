10498b04885888d677d02a8a5fefe696aaae2bb1
MichiVIP <michi.playstore@gmail.com>
2017-09-02 01:19:29 +0200

    Initialize basic Log4J structure

--- src/main/java/de/devgruppe/fundiscordbot/FunDiscordBotStarter.java
+++ src/main/java/de/devgruppe/fundiscordbot/FunDiscordBotStarter.java
@@ -1,5 +1,11 @@
 package de.devgruppe.fundiscordbot;
 
-public class FunDiscordBotStarter {
+import org.apache.log4j.Logger;
 
+public class FunDiscordBotStarter {
+  private static Logger logger;
+
+  public static void main(String[] args) {
+    logger = Logger.getLogger("FunDiscordBot");
+  }
 }
