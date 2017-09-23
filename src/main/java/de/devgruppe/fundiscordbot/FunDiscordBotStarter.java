de942e51ce948aa0a0fcf363c286d168cb2eb659
Conner <zargor3@gmail.com>
2017-09-23 23:59:24 +0200

    Added reflection register of commands

--- src/main/java/de/devgruppe/fundiscordbot/FunDiscordBotStarter.java
+++ src/main/java/de/devgruppe/fundiscordbot/FunDiscordBotStarter.java
@@ -1,15 +1,15 @@
 package de.devgruppe.fundiscordbot;
 
+import com.google.common.reflect.ClassPath;
+import de.devgruppe.fundiscordbot.command.Command;
 import de.devgruppe.fundiscordbot.command.CommandRegistry;
-import de.devgruppe.fundiscordbot.command.commands.CommandListCommand;
-import de.devgruppe.fundiscordbot.command.commands.CountCommand;
 import de.devgruppe.fundiscordbot.command.commands.EchoCommand;
-import de.devgruppe.fundiscordbot.command.commands.MemeCommand;
-import de.devgruppe.fundiscordbot.command.commands.MemeNamesCommand;
-import de.devgruppe.fundiscordbot.command.commands.RandomCommand;
 import de.devgruppe.fundiscordbot.command.impl.DefaultCommandRegistry;
 import de.devgruppe.fundiscordbot.config.Config;
 import de.devgruppe.fundiscordbot.config.Configuration;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import javax.security.auth.login.LoginException;
 import lombok.Getter;
@@ -25,81 +25,92 @@ import org.apache.log4j.Logger;
 
 public class FunDiscordBotStarter implements EventListener {
 
-  @Getter
-  private static FunDiscordBotStarter instance;
-  @Getter
-  private JDA jda;
-  @Getter
-  private Configuration configuration;
-  @Getter
-  private Config config;
-  @Getter
-  private CommandRegistry commandRegistry;
-  private static Logger logger;
+	@Getter
+	private static FunDiscordBotStarter instance;
+	@Getter
+	private JDA jda;
+	@Getter
+	private Configuration configuration;
+	@Getter
+	private Config config;
+	@Getter
+	private CommandRegistry commandRegistry;
+	private static Logger logger;
 
-  private FunDiscordBotStarter() {
-    configuration = new Configuration();
-    if (!configuration.exists()) {
-      Config config = new Config();
-      configuration.writeConfiguration(config);
-      logger.info("Config created! Program stopped!");
-      System.exit(0);
-    }
-    configuration.readConfiguration();
-    config = configuration.getConfig();
-    this.commandRegistry = new DefaultCommandRegistry();
-    logger.info("Connecting...");
-    try {
-      jda = new JDABuilder(AccountType.BOT)
-          .setToken(this.config.getBotToken())
-          .setAutoReconnect(true)
-          .addEventListener(this)
-          .setGame(Game.of("https://github.com/Dev-Gruppe/FunDiscordBot", "https://github.com/Dev-Gruppe/FunDiscordBot"))
-          .buildAsync();
-    } catch (LoginException | RateLimitedException e) {
-      e.printStackTrace();
-    }
-    if (jda == null) {
-      logger.error("JDA is null");
-      System.exit(0);
-    }
-    this.jda.addEventListener(this.commandRegistry);
-    this.registerCommands();
-  }
+	private FunDiscordBotStarter() {
+		configuration = new Configuration();
+		if (!configuration.exists()) {
+			Config config = new Config();
+			configuration.writeConfiguration(config);
+			logger.info("Config created! Program stopped!");
+			System.exit(0);
+		}
+		configuration.readConfiguration();
+		config = configuration.getConfig();
+		this.commandRegistry = new DefaultCommandRegistry();
+		logger.info("Connecting...");
+		try {
+			jda = new JDABuilder(AccountType.BOT)
+					.setToken(this.config.getBotToken())
+					.setAutoReconnect(true)
+					.addEventListener(this)
+					.setGame(Game.of("https://github.com/Dev-Gruppe/FunDiscordBot", "https://github.com/Dev-Gruppe/FunDiscordBot"))
+					.buildAsync();
+		} catch (LoginException | RateLimitedException e) {
+			e.printStackTrace();
+		}
+		if (jda == null) {
+			logger.error("JDA is null");
+			System.exit(0);
+		}
+		this.jda.addEventListener(this.commandRegistry);
+		this.registerCommands();
+	}
 
-  public static void main(String[] args) {
-    logger = Logger.getLogger("FunDiscordBot");
-    instance = new FunDiscordBotStarter();
-  }
+	public static void main(String[] args) {
+		logger = Logger.getLogger("FunDiscordBot");
+		instance = new FunDiscordBotStarter();
+	}
 
-  private void registerCommands(){
-    this.commandRegistry.registerCommand(new CommandListCommand());
-    this.commandRegistry.registerCommand(new EchoCommand());
-    this.commandRegistry.registerCommand(new RandomCommand());
-    this.commandRegistry.registerCommand(new CountCommand());
-    this.commandRegistry.registerCommand(new MemeCommand());
-    this.commandRegistry.registerCommand(new MemeNamesCommand());
-  }
+	private void registerCommands() {
+		registerCommandsInPackage(EchoCommand.class.getPackage().getName());
+	}
 
-  @Override
-  public void onEvent(Event event) {
-    if (event instanceof ReadyEvent) {
-      logger.info("Connected");
-      new Thread(() -> {
-        Scanner scanner = new Scanner(System.in);
-        String line;
-        while ((line = scanner.nextLine()) != null) {
-          if (line.equals("stop")) {
-            stopBot(event);
-          }
-        }
-      }).start();
-    }
-  }
+	@Override
+	public void onEvent(Event event) {
+		if (event instanceof ReadyEvent) {
+			logger.info("Connected");
+			new Thread(() -> {
+				Scanner scanner = new Scanner(System.in);
+				String line;
+				while ((line = scanner.nextLine()) != null) {
+					if (line.equals("stop")) {
+						stopBot(event);
+					}
+				}
+			}).start();
+		}
+	}
 
-  private void stopBot(Event event) {
-    event.getJDA().shutdown();
-    System.exit(0);
-  }
+	private void stopBot(Event event) {
+		event.getJDA().shutdown();
+		System.exit(0);
+	}
 
+	private void registerCommandsInPackage(String p) {
+		try {
+			List<Class> classes = new ArrayList<>();
+			for (ClassPath.ClassInfo classInfo : ClassPath.from(getClass().getClassLoader()).getTopLevelClassesRecursive(p)) {
+				Class clazz = Class.forName(classInfo.getName());
+				if (clazz.getSuperclass().getName().equals(Command.class.getName())) {
+					classes.add(clazz);
+				}
+			}
+			for (Class clazz : classes) {
+				this.commandRegistry.registerCommand((Command) clazz.newInstance());
+			}
+		} catch (IOException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+			e.printStackTrace();
+		}
+	}
 }
