1711b45f9ebc7a11095b4180cc41b9d59e13f765
MichiVIP <michi.developer@gmail.com>
2017-10-02 01:48:49 +0200

    Implement basic MessageEmbed object.

--- src/main/java/de/devgruppe/fundiscordbot/command/commands/CommandListCommand.java
+++ src/main/java/de/devgruppe/fundiscordbot/command/commands/CommandListCommand.java
@@ -4,30 +4,28 @@ import de.devgruppe.fundiscordbot.FunDiscordBotStarter;
 import de.devgruppe.fundiscordbot.command.Command;
 import de.devgruppe.fundiscordbot.command.CommandRegistry;
 import de.devgruppe.fundiscordbot.command.CommandResponse;
+
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
+
+import java.awt.*;
 
 /**
  * Created by GalaxyHD on 12.09.2017.
  */
 public class CommandListCommand extends Command {
-
   public CommandListCommand() {
-    super("commandlist", "", "Gibt dir eine Übersicht von allen Befehlen");
+    super("commandlist", "", "Gibt dir eine Übersicht von allen Befehlen aus.");
   }
 
   @Override
   public CommandResponse triggerCommand(Message message, String[] args) {
-    StringBuilder stringBuilder = new StringBuilder();
-    CommandRegistry commandRegistry = FunDiscordBotStarter.getInstance().getCommandRegistry();
-    stringBuilder.append("Befehle: \n");
-    stringBuilder.append("```");
-    commandRegistry.getRegisteredCommands().forEach(command -> stringBuilder.append(commandRegistry.getPrefix())
-        .append(command.getCommandName())
-        .append(" - ")
-        .append(command.getDescription())
-        .append("\n"));
-    stringBuilder.append("```");
-    message.getTextChannel().sendMessage(stringBuilder.toString()).complete();
+    final CommandRegistry commandRegistry = FunDiscordBotStarter.getInstance().getCommandRegistry();
+    final EmbedBuilder embedBuilder = new EmbedBuilder().setColor(Color.GRAY).setTitle("Befehlsübersicht");
+    commandRegistry.getRegisteredCommands().forEach(command ->
+            embedBuilder.addField(commandRegistry.getPrefix() + command.getCommandName(), command.getDescription(), false));
+    //embedBuilder.setFooter("Seite 1", null);
+    message.getTextChannel().sendMessage(embedBuilder.build()).queue();
     return CommandResponse.ACCEPTED;
   }
 }
