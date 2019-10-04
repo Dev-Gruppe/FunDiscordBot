64779224e1c930a6a1eba0b6c5fc8876bc374f9a
Markus <m.kording@hotmail.de>
2019-10-04 16:52:20 +0200

    Update to gradle (#16)
    * Update to gradle
    
    * Add StatusCommand

--- src/main/java/de/devgruppe/fundiscordbot/FunDiscordBotStarter.java
+++ src/main/java/de/devgruppe/fundiscordbot/FunDiscordBotStarter.java
@@ -1,11 +1,7 @@
 package de.devgruppe.fundiscordbot;
 
 import de.devgruppe.fundiscordbot.command.CommandRegistry;
-import de.devgruppe.fundiscordbot.command.commands.CommandListCommand;
-import de.devgruppe.fundiscordbot.command.commands.CountCommand;
-import de.devgruppe.fundiscordbot.command.commands.EchoCommand;
-import de.devgruppe.fundiscordbot.command.commands.PingCommand;
-import de.devgruppe.fundiscordbot.command.commands.RandomCommand;
+import de.devgruppe.fundiscordbot.command.commands.*;
 import de.devgruppe.fundiscordbot.command.commands.giphy.RandomGiphyCommand;
 import de.devgruppe.fundiscordbot.command.commands.giphy.TrendingGiphyCommand;
 import de.devgruppe.fundiscordbot.command.commands.memegen.MemeGenCommand;
@@ -94,6 +90,7 @@ public class FunDiscordBotStarter implements EventListener {
     this.commandRegistry.registerCommand(new RandomGiphyCommand());
     this.commandRegistry.registerCommand(new TrendingGiphyCommand());
     this.commandRegistry.registerCommand(new PingCommand());
+    this.commandRegistry.registerCommand(new StatusCommand());
     commandListCommand.initialize();
   }
 
